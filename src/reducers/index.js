a485755833d3ada8f450def260d550b098ef813f
Lenny Sirivong <lenny@lenny-s.com>
2017-02-04 19:58:56 -0500

    Remove routines reducer ref

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-import routines from './routines';
 import currentUser from './user';
 
 const gymBuddy = combineReducers({
-  routines,
   currentUser
 });
 
